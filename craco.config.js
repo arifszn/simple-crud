b5ea41b13149e43472112c840d07921eb72413a0
Ariful Alam <arif@orangetoolz.com>
2021-03-01 16:47:11 +0600

    Add dynamic theme

--- craco.config.js
+++ craco.config.js
@@ -1,15 +1,27 @@
 const CracoAntDesignPlugin = require("craco-antd");
+const path = require('path');
+const { generateTheme } = require('antd-theme-generator');
+
+const themeGeneratorOptions = {
+    antDir: path.join(__dirname, './node_modules/antd'),
+    stylesDir: path.join(__dirname, './src/assets/less'),
+    varFile: path.join(__dirname, './src/assets/less/ant_variables.less'),
+    themeVariables: ['@primary-color'],
+    outputFilePath: path.join(__dirname, './public/assets/less/antd.less'),
+}
+
+generateTheme(themeGeneratorOptions).then(less => {
+  console.log('Theme generated successfully');
+})
+.catch(error => {
+  console.log('Error', error);
+})
 
 /* craco.config.js */
 module.exports = {
     plugins: [
         {
             plugin: CracoAntDesignPlugin,
-            options: {
-                customizeTheme: {
-                    "@primary-color": "#1DA57A",
-                },
-            },
         },
     ],
 };
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,6 +1,12 @@
 import React, { useEffect } from 'react';
 
 const Dashboard = () => {
+    useEffect(() => {
+        window.less.modifyVars({
+            '@primary-color': '#FF0000'
+        })
+    }, [])
+
     return (
         <React.Fragment>
             Dashboard component
